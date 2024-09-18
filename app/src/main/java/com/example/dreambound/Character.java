d4e44543f0a004767528e7935e9f644c7ec9647b
thefool309 <themagician309@gmail.com>
2024-09-18 01:07:58 -0500

    Added battle trigger and default character stats. Will need more logic for battle before I can pass information back to main activity on win or loss. Will also need UI and logic for battle screen.

--- app/src/main/java/com/example/dreambound/Character.java
+++ app/src/main/java/com/example/dreambound/Character.java
@@ -8,13 +8,15 @@ public class Character extends GameObject {
         int Defense = 10;
         int SpAttack = 10;
         int SpDefense = 10;
+        int Health = 50;
     }
 
     Stats stats;
     //Constructor
     Character(float x, float y, float width, float height) {
         super(x, y, width, height);
-        Stats stats = new Stats();
+
+        stats = new Stats(); //default character Stats
     }
 }
 
