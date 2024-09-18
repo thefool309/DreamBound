b8d3ba980cfc955a85cd427805225e4e4e5c4e88
thefool309 <themagician309@gmail.com>
2024-09-18 03:38:03 -0500

    added statsBIN data structure to make File I/O and Saving data easier.

--- app/src/main/java/com/example/dreambound/Character.java
+++ app/src/main/java/com/example/dreambound/Character.java
@@ -2,15 +2,17 @@ package com.example.dreambound;
 
 
 public class Character extends GameObject {
-
+    //Stats class
     public static class Stats{
         int Attack = 10;
         int Defense = 10;
         int SpAttack = 10;
         int SpDefense = 10;
         int Health = 50;
+        //stats stored in BIN for easy file I/O
+        byte[] statsBIN = {(byte) Attack, (byte) Defense, (byte)SpAttack, (byte)SpDefense, (byte)Health };
     }
-
+    //stats variable
     Stats stats;
     //Constructor
     Character(float x, float y, float width, float height) {
---
+++ app/src/main/java/com/example/dreambound/CollisionHandler.java
@@ -0,0 +1,14 @@
+package com.example.dreambound;
+
+public class CollisionHandler {
+    //TODO: Data structure for collide-able entities
+
+    //TODO: Data structure for Creature Entities
+
+    //TODO: Constructor generate our tilemaps
+    CollisionHandler() {
+
+    }
+
+    //TODO: traverse our data structure and find objects from position quickly
+}
