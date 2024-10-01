233d22d8555765ceea3b636f21cb3a26b26d32ec
thefool309 <themagician309@gmail.com>
2024-10-01 17:33:58 -0500

    added constant GID_MASK to Constants.java

--- app/src/main/java/com/example/dreambound/Constants.java
+++ app/src/main/java/com/example/dreambound/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String TAG = "DreamBound";
     public static final float CHUNK_SIZE = 6.4E01f;
     public static final int DELAY = 17;
+    public static final long GID_MASK = 0x3fffffff;
 }
