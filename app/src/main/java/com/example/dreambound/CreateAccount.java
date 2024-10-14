eb17c2a4e3cb9bec63dc8326fbdcb42fe362f1a0
kehowell92 <138188533+kehowell92@users.noreply.github.com>
2024-10-13 19:33:45 -0500

    Create account linked to FireBase and testable

--- app/src/main/java/com/example/dreambound/CreateAccount.java
+++ app/src/main/java/com/example/dreambound/CreateAccount.java
@@ -1,20 +1,88 @@
 package com.example.dreambound;
 
+import android.content.Intent;
+import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
 
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import org.jetbrains.annotations.NotNull;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
+import static com.example.dreambound.R.*;
 
 public class CreateAccount extends AppCompatActivity {
 
+    EditText mEmail, mPassword;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_account);
+        mEmail = findViewById(R.id.email_input);
+        mPassword = findViewById(R.id.password_text);
+        mRegisterBtn = findViewById(id.register_button);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    mPassword.setError("Password must be at least 6 characters");
+                    return;
+                }
+
+                //register user in Firebase
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(CreateAccount.this, "Account created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else
+                        {
+                            Toast.makeText(CreateAccount.this, "Error!" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/dreambound/FirebaseAuth.java
@@ -0,0 +1,4 @@
+package com.example.dreambound;
+
+public class FirebaseAuth {
+}
