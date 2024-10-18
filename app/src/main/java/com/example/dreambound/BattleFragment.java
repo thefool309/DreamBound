11be809d4f396875b582a1e12670d4456a752cc2
Ghostcode22 <134975339+Ghostcode22@users.noreply.github.com>
2024-10-18 17:16:56 -0500

    End of battle trainsition back too game fragment added

--- app/src/main/java/com/example/dreambound/BattleFragment.java
+++ app/src/main/java/com/example/dreambound/BattleFragment.java
@@ -3,7 +3,6 @@ package com.example.dreambound;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
